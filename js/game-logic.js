523ae08b1dad34573c79f41b036a49277d04e324
Daryl <daryl.villalobos@hotmail.com>
2022-06-26 15:00:16 +0900

    how do I test this?

--- js/game-logic.js
+++ js/game-logic.js
@@ -13,3 +13,22 @@ let playerOneMoveOneType,
     playerTwoMoveTwoValue,
     playerTwoMoveThreeType, 
     playerTwoMoveThreeValue;
+
+let playerOneArr = [playerOneMoveOneType, playerOneMoveOneValue, playerOneMoveTwoType, playerOneMoveTwoValue, playerOneMoveThreeType, playerOneMoveThreeValue];
+let playerTwoArr = [playerTwoMoveOneType, playerTwoMoveOneValue, playerTwoMoveTwoType, playerTwoMoveTwoValue, playerTwoMoveThreeType, playerTwoMoveThreeValue];
+
+function setPlayerMoves(player, moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue){
+    let playerMovesArr = [moveOneType, moveOneValue, moveTwoType, moveTwoValue, moveThreeType, moveThreeValue];
+    if(player == 'Player One'){
+        for(let i = 0; i < playerMovesArr.length; i++){
+            playerOneArr[i] = playerMovesArr[i];
+        };
+    } else if(player == 'Player Two'){
+        for(let i = 0; i < playerMovesArr.length; i++){
+            playerTwoArr[i] = playerMovesArr[i];
+        };
+    } else {
+        console.log('Invalid player entered')
+    };
+}
+
