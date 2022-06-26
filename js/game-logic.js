ba24981241939b068ac88d278911b3729b9504a2
Daryl <daryl.villalobos@hotmail.com>
2022-06-26 12:38:47 +0900

    first save

---
+++ js/game-logic.js
@@ -0,0 +1,15 @@
+// All code should be written in this file.
+
+//12 global variables
+let playerOneMoveOneType, 
+    playerOneMoveOneValue, 
+    playerOneMoveTwoType,
+    playerOneMoveTwoValue,
+    playerOneMoveThreeType,
+    playerOneMoveThreeValue,
+    playerTwoMoveOneType, 
+    playerTwoMoveOneValue,
+    playerTwoMoveTwoType, 
+    playerTwoMoveTwoValue,
+    playerTwoMoveThreeType, 
+    playerTwoMoveThreeValue;
